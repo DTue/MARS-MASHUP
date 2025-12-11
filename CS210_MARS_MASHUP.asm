00ba079aef8b18d024f94249abb07c7502bebb69
Tue(Dolphin) Tran <tuetamtran2307@gmail.com>
2025-12-11 14:56:47 -0800

    Update

--- CS210_MARS_MASHUP.asm
+++ CS210_MARS_MASHUP.asm
@@ -6,8 +6,11 @@
 xmas
 
 play: 
+bsti $t5, $t5, 1
 #Feature 2: Asks dj for a random track
 dj
+dsn $t6, $t5, play
+
 
 #Feature 3: Blend two registers together to get a new tune(value) - Find an average value between two registers
 bsti $t0, $zero, 10
@@ -16,17 +19,22 @@ bl $t2, $t0, $t1
 #dsn $t1, $t0, play
 
 #Feature 4: Crossfade - cross/swap two registers 
-#cf $t1, $t2
+cf $t1, $t2
 
 #Feature 5: SHHH: mute all of your registers by setting all to 0
 #mut $t0, $t1, $t2
 
 #Feature 6: Get your notes/registers value
-div $t2, $t1
-flt $t3
-shrp $t4
+#dcy $t2, $t1
+#flt $t3
+#shrp $t4
 
 
+#Feature 7: Sync your registers: set values the same
+sync $t0, $t1, $t2
+
+#Feature 8: Unlimited Play until program crash =)
+#h play
 
 
 
